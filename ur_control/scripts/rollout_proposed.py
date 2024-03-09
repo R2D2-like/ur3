8d99589c939164939cb6e28c7138f4a2419f50cb
R2D2-like <tsukurobokun@gmail.com>
2024-03-10 05:09:47 +0900

    add proposed rollout

---
+++ ur_control/scripts/rollout_proposed.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+from ur_control.arm import Arm
+import rospy
+import numpy as np
+import os
+import sys
+# add /root/Research_Internship_at_GVlab/scripts/config
+sys.path.append('/root/Research_Internship_at_GVlab/scripts/config')
+from values import SCALING_FACTOR, DEMO_TRAJECTORY_MIN, DEMO_TRAJECTORY_MAX, DEMO_FORCE_TORQUE_MIN, DEMO_FORCE_TORQUE_MAX
+import torch
+sys.path.append('/root/Research_Internship_at_GVlab/scripts/train/')
+from lfd_proposed import LfdProposed
+from ur_control import transformations
+import collections
+from geometry_msgs.msg import Pose
+
+class Rollout:
+    def __init__(self) -> None:
+        rospy.init_node('rollout', anonymous=True)
+        self.arm = Arm(gripper_type=None, ee_link='wrist_3_link') # with gripper
+        
+        # Subscriber
+        self.sub_eef_pose = rospy.Subscriber('/eef_pose', Pose, self.eef_pose_callback)
+        self.eef_position_history = collections.deque(maxlen=100)
+
+        self.save_dir = '/root/Research_Internship_at_GVlab/'
+        self.save_name = input('Enter the name of the save file for inference results: ')
+
+        exploratory_data_name = input('Enter the name of the exploratory data file: ')
+        self.vae_inputs = np.load(self.save_dir + 'real/rollout/data/exploratory/'+ exploratory_data_name + '.npy') # normalized
+
+        model_weights_path = self.save_dir + 'real/model/propose.pth'
+        self.lfd = LfdProposed()
+        self.lfd.load_state_dict(torch.load(model_weights_path))
+        self.lfd.eval()
+        rospy.loginfo('Rollout node initialized')
+
+    def eef_pose_callback(self, msg):
+        current_pos = np.array([msg.position.x, msg.position.y, msg.position.z])
+        self.eef_position_history.append(current_pos)
+
+
+    def move_endeffector(self, deltax, target_time):
+        # get current position of the end effector
+        cpose = self.arm.end_effector()
+        # define the desired translation/rotation
+        deltax = np.array(deltax)
+        # add translation/rotation to current position
+        cpose = transformations.transform_pose(cpose, deltax, rotated_frame=True)
+        # execute desired new pose
+        # may fail if IK solution is not found
+        self.arm.set_target_pose(pose=cpose, wait=True, target_time=target_time)
+
+    def init_pressing(self):
+        self.arm.zero_ft_sensor()
+        self.move_endeffector([0, 0, 0.01, 0, 0, 0], target_time=1)
+
+    def output2position(self, normalized_output):
+        # 正規化された出力をもとの値に戻す
+        # normalized_output = np.load(self.save_dir + self.save_name) #(2000, 3)
+        normalized_output /= SCALING_FACTOR
+        output = normalized_output * (DEMO_TRAJECTORY_MAX - DEMO_TRAJECTORY_MIN) + DEMO_TRAJECTORY_MIN
+        return output
+    
+    def position2input(self, position):
+        # 位置をモデルの入力に変換
+        input = (position - DEMO_TRAJECTORY_MIN) / (DEMO_TRAJECTORY_MAX - DEMO_TRAJECTORY_MIN)
+        input *= SCALING_FACTOR
+        return input
+    
+    def ft2input(self, ft):
+        # 力覚センサの値をモデルの入力に変換
+        input = (ft - DEMO_FORCE_TORQUE_MIN) / (DEMO_FORCE_TORQUE_MAX - DEMO_FORCE_TORQUE_MIN)
+        return input
+    
+    def predict(self, vae_inputs, tcn_inputs):
+        # inference
+        output = self.lfd(vae_inputs, tcn_inputs)
+        output = output.detach().numpy()
+        eef_position = self.output2position(output)
+        return eef_position
+
+
+    def rollout(self):
+        start_time = rospy.Time.now()
+        # initialize motion
+        self.init_pressing()
+
+        while (rospy.Time.now() - start_time).to_sec() < 20:
+            ft_history = self.arm.get_wrench_history(hist_size=100)
+            ft_history = self.ft2input(ft_history) # (100, 6)
+            eef_position_history = self.position2input(self.eef_position_history) # (100, 3)
+            tcn_inputs = np.concatenate([ft_history, eef_position_history], axis=1) # (100, 9)
+            # (100, 9) -> (9, 100)
+            tcn_inputs = tcn_inputs.T
+            next_eef_position = self.predict(self.vae_inputs, tcn_inputs)
+            
+            target_time = 0.01
+
+            for i in range(next_eef_position.shape[0]):
+                pose_goal = self.arm.end_effector()
+                pose_goal[0] = next_eef_position[i, 0]
+                pose_goal[1] = next_eef_position[i, 1]
+                pose_goal[2] = next_eef_position[i, 2]
+                try:
+                    self.arm.set_target_pose(pose=pose_goal, wait=True, target_time=target_time)
+                except Exception as e:
+                    print(e)
+
+        rospy.loginfo('Rollout completed')
+
+if __name__ == '__main__':
+    rollout = Rollout()
+    rollout.rollout()
+        

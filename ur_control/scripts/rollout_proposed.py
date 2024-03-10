476511a64da308cd963ea51eaa62e2b14ea23879
R2D2-like <tsukurobokun@gmail.com>
2024-03-11 06:24:06 +0900

    add rollout for proposal

--- ur_control/scripts/rollout_proposed.py
+++ ur_control/scripts/rollout_proposed.py
@@ -11,33 +11,35 @@ import torch
 sys.path.append('/root/Research_Internship_at_GVlab/scripts/train/')
 from lfd_proposed import LfdProposed
 from ur_control import transformations
-import collections
 from geometry_msgs.msg import Pose
 
-class Rollout:
+class RolloutProposed:
     def __init__(self) -> None:
         rospy.init_node('rollout', anonymous=True)
         self.arm = Arm(gripper_type=None, ee_link='wrist_3_link') # with gripper
         
         # Subscriber
         self.sub_eef_pose = rospy.Subscriber('/eef_pose', Pose, self.eef_pose_callback)
-        self.eef_position_history = collections.deque(maxlen=100)
+        self.eef_pose_history = []
 
-        self.save_dir = '/root/Research_Internship_at_GVlab/'
-        self.save_name = input('Enter the name of the save file for inference results: ')
+        self.base_dir = '/root/Research_Internship_at_GVlab/real/'
+        stiffness = input('stiffness level (1, 2, 3, 4): ')
+        friction = input('friction level (1, 2, 3): ')
+        self.sponge = 's' + stiffness + 'f' + friction
+        self.base_save_dir = self.base_dir + 'rollout/data/'
 
-        exploratory_data_name = input('Enter the name of the exploratory data file: ')
-        self.vae_inputs = np.load(self.save_dir + 'real/rollout/data/exploratory/'+ exploratory_data_name + '.npy') # normalized
+        self.vae_inputs = np.load(self.base_dir + 'rollout/data/exploratory/exploratory_action_preprocessed.npz')[self.sponge] # normalized
 
-        model_weights_path = self.save_dir + 'real/model/propose.pth'
+        model_weights_path = self.base_dir + 'model/proposed/proposed_model.pth'
         self.lfd = LfdProposed()
         self.lfd.load_state_dict(torch.load(model_weights_path))
         self.lfd.eval()
         rospy.loginfo('Rollout node initialized')
 
     def eef_pose_callback(self, msg):
-        current_pos = np.array([msg.position.x, msg.position.y, msg.position.z])
-        self.eef_position_history.append(current_pos)
+        current_pos = np.array([msg.position.x, msg.position.y, msg.position.z, \
+                                msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w])
+        self.eef_pose_history.append(current_pos)
 
 
     def move_endeffector(self, deltax, target_time):
@@ -57,7 +59,7 @@ class Rollout:
 
     def output2position(self, normalized_output):
         # 正規化された出力をもとの値に戻す
-        # normalized_output = np.load(self.save_dir + self.save_name) #(2000, 3)
+        # normalized_output = np.load(self.base_dir + self.save_name) #(2000, 3)
         normalized_output /= SCALING_FACTOR
         output = normalized_output * (DEMO_TRAJECTORY_MAX - DEMO_TRAJECTORY_MIN) + DEMO_TRAJECTORY_MIN
         return output
@@ -89,7 +91,7 @@ class Rollout:
         while (rospy.Time.now() - start_time).to_sec() < 20:
             ft_history = self.arm.get_wrench_history(hist_size=100)
             ft_history = self.ft2input(ft_history) # (100, 6)
-            eef_position_history = self.position2input(self.eef_position_history) # (100, 3)
+            eef_position_history = self.position2input(self.eef_pose_history[-100:][:3]) # (100, 3)
             tcn_inputs = np.concatenate([ft_history, eef_position_history], axis=1) # (100, 9)
             # (100, 9) -> (9, 100)
             tcn_inputs = tcn_inputs.T
@@ -107,9 +109,19 @@ class Rollout:
                 except Exception as e:
                     print(e)
 
+        # save data
+        traj_history = self.eef_pose_history[-2000:] # (2000, 7)
+        ft_history = self.arm.get_wrench_history(hist_size=2000) # (2000, 6)
+        save_dir = self.base_save_dir + 'result/proposed/'
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        save_path = save_dir + self.sponge + '.npz'
+        np.savez(save_path, pose=traj_history, ft=ft_history)
+        rospy.loginfo('Data saved at\n' + save_path)
+
         rospy.loginfo('Rollout completed')
 
 if __name__ == '__main__':
-    rollout = Rollout()
-    rollout.rollout()
+    rollout_proposed = RolloutProposed()
+    rollout_proposed.rollout()
         
